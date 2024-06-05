7a964548f3a888295b156f48d5083c675beab6a0
Vako <valeri.mekhashishvili97@gmail.com>
2024-06-05 12:01:31 +0400

    Initial Commit

---
+++ Themoviedb/ThemoviedbApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemoviedbApp.swift
+//  Themoviedb
+//
+//  Created by valeri mekhashishvili on 05.06.24.
+//
+
+import SwiftUI
+
+@main
+struct ThemoviedbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
