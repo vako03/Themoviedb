2705e5622f97b8696b49d37c37db7af373c56a19
Vako <valeri.mekhashishvili97@gmail.com>
2024-06-07 07:06:11 +0400

    change tabview icons

--- Themoviedb/Views/MainTabView.swift
+++ Themoviedb/Views/MainTabView.swift
@@ -9,19 +9,29 @@ import SwiftUI
 
 
 struct MainTabView: View {
+    @State private var selection = 0 // State to track selected tab
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selection) {
             MoviesCollectionView()
                 .tabItem {
-                    Label("Home", image: "Home")
+                    Image("Home") // Use your custom image from the asset catalog
+                        .renderingMode(.template) // Use renderingMode to allow color changes
+                        .foregroundColor(selection == 0 ? Color.blue : Color.gray) // Change color based on selection
+                    Text("Home")
                 }
+                .tag(0) // Tag this view with index 0
             
             SearchView()
                 .tabItem {
-                    Label("Search", image: "Search")
+                    Image("Search") // Use your custom image from the asset catalog
+                        .renderingMode(.template) // Use renderingMode to allow color changes
+                        .foregroundColor(selection == 1 ? Color.blue : Color.gray) // Change color based on selection
+                    Text("Search")
                 }
+                .tag(1) // Tag this view with index 1
         }
-        .appBackground() // Apply background here
         .navigationViewStyle(StackNavigationViewStyle())
+        
     }
 }
